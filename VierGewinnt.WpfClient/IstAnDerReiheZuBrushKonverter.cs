ca8132e98642e833d6be90e5070dd7224ea0e5e1
Rolf Jansen <rolf.jansen@polizei.nrw.de>
2019-02-11 14:43:33 +0100

    Video08 Fertig

---
+++ VierGewinnt.WpfClient/IstAnDerReiheZuBrushKonverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace VierGewinnt.WpfClient
+{
+    public class IstAnDerReiheZuBrushKonverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var spielerViewModel = value as ISpielerViewModel;
+            if (spielerViewModel == null)
+                throw new ArgumentException($"{nameof(value)} ist kein {nameof(ISpielerViewModel)}");
+
+            if (spielerViewModel.IstAnDerReihe)
+            {
+                var spielerFarbe = spielerViewModel.Spieler.SpielerFarbe;
+                return new SolidColorBrush(Color.FromRgb(spielerFarbe.Rot, spielerFarbe.Grün, spielerFarbe.Blau));
+            }
+
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
