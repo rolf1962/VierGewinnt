37a904cac68145b1ca1fd5264cac72123695e357
MAMAPAPA\Rolf <jansen.rolf@t-online.de>
2019-05-12 18:21:35 +0200

    Noch zum voeherihgen Commit

--- VierGewinnt.WpfClient/IMainWindowViewModel.cs
+++ VierGewinnt.WpfClient/IMainWindowViewModel.cs
@@ -6,7 +6,6 @@ namespace VierGewinnt.WpfClient
 {
     public interface IMainWindowViewModel : INotifyPropertyChanged
     {
-        // Video 12: Unterbrechung bei Video-Position 08:15
         ISpielbrettViewModel SpielbrettViewModel { get; }
         IReadOnlyList<ISpielerViewModel> SpielerViewModels { get; }
         string Gewinnername { get; }
